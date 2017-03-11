05fd28abe56de4b170a6a5facf7fd7612ae76e5d
gadzik <glina41@o2.pl>
2017-03-11 15:26:05 +0100

    account

---
+++ gadziksy-ejb/src/main/java/jpa/Account.java
@@ -0,0 +1,66 @@
+package jpa;
+
+import converter.LocalDateAdapter;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.time.LocalDate;
+
+/**
+ * Created by gadzik on 11.03.17.
+ */
+@Entity
+@Table(name = "account")
+public class Account {
+
+    @SequenceGenerator(name="ACCOUNT_SEQ" , sequenceName = "account_id_seq", allocationSize = 1)
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "ACCOUNT_SEQ", strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "creation_date")
+    private LocalDate creationDate;
+
+    public Account() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @XmlJavaTypeAdapter(LocalDateAdapter.class)
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+}
