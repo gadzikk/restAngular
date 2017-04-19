a21744a08a383ee7740bf73a8a868661380556e5
gadzik <glina41@o2.pl>
2017-04-19 20:21:36 +0200

    trigger approach finished

--- gadziksy-ejb/src/main/java/service/AccountServiceBean.java
+++ gadziksy-ejb/src/main/java/service/AccountServiceBean.java
@@ -25,15 +25,21 @@ public class AccountServiceBean implements AccountService {
     @Override
     public void prepareTransfer(Account sender, Account receiver, BigDecimal transferedMoney) {
 
-        BigDecimal senderMoney = sender.getMoney().subtract(transferedMoney);
-        sender.setMoney(senderMoney);
-        accountRepository.updateAccount(sender);
-
-        BigDecimal receiverMoney = receiver.getMoney().add(transferedMoney);
-        receiver.setMoney(receiverMoney);
-        accountRepository.updateAccount(receiver);
-
         Transfer transfer = new Transfer(sender, receiver, transferedMoney);
         transferRepository.writeTransfer(transfer);
+
+//        CREATE FUNCTION doTransfer() RETURNS trigger AS $$
+//        BEGIN
+//        UPDATE ACCOUNT SET MONEY = MONEY - NEW.transfered_money WHERE id = NEW.SENDER_ID;
+//        UPDATE ACCOUNT SET MONEY = MONEY + NEW.transfered_money WHERE id = NEW.RECEIVER_ID;
+//        RETURN NULL;
+//        END;
+//        $$ language plpgsql;
+//
+//        CREATE TRIGGER transfertrg
+//        AFTER INSERT
+//        ON transfer
+//        FOR EACH ROW
+//        EXECUTE PROCEDURE doTransfer();
     }
 }
